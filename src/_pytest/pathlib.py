31985239774c15e87f114effef26c1cfea1113fb
Manuel Jacob <me@manueljacob.de>
2023-02-17 06:49:15 +0100

    Make import_path() return already imported module when import mode is importlib.

--- src/_pytest/pathlib.py
+++ src/_pytest/pathlib.py
@@ -504,6 +504,8 @@ def import_path(
 
     if mode is ImportMode.importlib:
         module_name = module_name_from_path(path, root)
+        if module_name in sys.modules:
+            return sys.modules[module_name]
 
         for meta_importer in sys.meta_path:
             spec = meta_importer.find_spec(module_name, [str(path.parent)])
